d96996d4b7015c2b15e9b4f5a6b7094de30a9464
Edwin DeNicholas <disruption999@hotmail.com>
2022-10-06 20:21:08 -0700

    Complete problem 3

--- problem_3/problem_3_solution.py
+++ problem_3/problem_3_solution.py
@@ -1,3 +1,17 @@
 def length_of_longest_substring(s: str) -> int:
     """Given a string s, find the length of the longest substring without repeating characters."""
-    return 0
+    current_substring = ''
+    len_longest_substring = 0
+    for char in s:
+        if char in current_substring:
+            len_current_substring = len(current_substring)
+            if len_current_substring > len_longest_substring:
+                len_longest_substring = len_current_substring
+            position_of_duplicate_char = current_substring.find(char)
+            current_substring = current_substring[position_of_duplicate_char+1:] + char
+        else:
+            current_substring += char
+    len_current_substring = len(current_substring)
+    if len_current_substring > len_longest_substring:
+        len_longest_substring = len_current_substring
+    return len_longest_substring
--- problem_3/test_problem_3_solution.py
+++ problem_3/test_problem_3_solution.py
@@ -5,9 +5,12 @@ from problem_3.problem_3_solution import length_of_longest_substring
 @pytest.mark.parametrize(
     "s, e",
     [
+        ('', 0),
         ('abcabcbb', 3),
         ('bbbbb', 1),
         ('pwwkew', 3),
+        ('dvdf', 3),
+        ('123456789101112131415', 10)
     ]
 )
 def test_problem_3_solution(s, e):
