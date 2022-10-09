6fb9116bae816f60bdc49b021b3c5d5a8d12ea0b
Edwin DeNicholas <disruption999@hotmail.com>
2022-10-08 18:57:34 -0700

    Problem 4 initial solution.

--- problem_4/problem_4_solution.py
+++ problem_4/problem_4_solution.py
@@ -1,19 +1,11 @@
-import pytest
-from problem_4.test_problem_4_solution import find_median_sorted_arrays
-
-
-@pytest.mark.parametrize(
-    "list1, list2, expected_median",
-    [
-        ([], [], 0),
-        ([], [5], 5),
-        ([5], [0], 5),
-        ([1, 3], [2], 2),
-        ([1, 2], [3, 4], 2.5),
-        ([3, 5, 7, 12, 21, 23, 23, 23, 23, 29, 40], [13, 14, 56], 22),
-        ([3, 13, 14, 21, 23, 23, 29, 39, 56], [5, 7, 12, 23, 23, 40], 23),
-    ]
-)
-def test_problem_4_solution(list1, list2, expected_median):
-    result = find_median_sorted_arrays(list1, list2)
-    assert result == expected_median
+def find_median_sorted_arrays(nums1, nums2):
+    nums = sorted(nums1 + nums2)
+    len_nums = len(nums)
+    if len_nums == 0:
+        return 0
+    if len_nums == 1:
+        return nums[0]
+    middle = len_nums // 2
+    if len_nums % 2 == 0:
+        return (nums[middle - 1] + nums[middle]) / 2
+    return nums[middle]
--- problem_4/test_problem_4_solution.py
+++ problem_4/test_problem_4_solution.py
@@ -1,2 +1,20 @@
-def find_median_sorted_arrays(nums1, nums2):
-    return 0
+import pytest
+from problem_4.problem_4_solution import find_median_sorted_arrays
+
+
+@pytest.mark.parametrize(
+    "list1, list2, expected_median",
+    [
+        ([], [], 0),
+        ([], [5], 5),
+        ([5], [], 5),
+        ([1, 3], [2], 2),
+        ([1, 2], [3, 4], 2.5),
+        ([3, 5, 7, 12, 21, 23, 23, 23, 23, 29, 40], [13, 14, 56], 22),
+        ([3, 13, 14, 21, 23, 23, 29, 39, 56], [5, 7, 12, 23, 23, 40], 23),
+    ]
+)
+def test_problem_4_solution(list1, list2, expected_median):
+    result = find_median_sorted_arrays(list1, list2)
+    assert result == expected_median
+
