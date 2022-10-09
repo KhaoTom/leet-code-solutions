412850db5ac933bef2b27b03d58f77bd0d93680c
Edwin DeNicholas <disruption999@hotmail.com>
2022-10-09 15:34:02 -0700

    Bring in first accepted solution for reference.

--- problem_5/problem_5_solution.py
+++ problem_5/problem_5_solution.py
@@ -31,3 +31,28 @@ def search_palindromes(s, search_length):
 
 def is_palindrome(s):
     return s == s[::-1]
+
+
+def first_solution(s) :
+    longest = s[0]
+    seen_values = {s[0]}
+    for i in range(1, len(s)):
+        if s[i] in seen_values:
+            candidate = find_longest_palindrome_starting_with_last_value(s[s.find(s[i]):i + 1])
+            if len(candidate) > len(longest):
+                longest = candidate
+        else:
+            seen_values.add(s[i])
+
+    return longest
+
+
+def find_longest_palindrome_starting_with_last_value(s):
+    last = s[-1]
+    longest = ''
+    for i in range(len(s)):
+        if s[i] == last:
+            candidate = s[i:]
+            if candidate == candidate[::-1] and len(candidate) > len(longest):
+                longest = candidate
+    return longest
