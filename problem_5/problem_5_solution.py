17bbc1fb05fdba8e7e1779af42452d66d15a67c0
Edwin DeNicholas <disruption999@hotmail.com>
2022-10-08 22:21:44 -0700

    Attempt to speed up when there are long sequences of repeated values.

--- problem_5/problem_5_solution.py
+++ problem_5/problem_5_solution.py
@@ -1,20 +1,34 @@
-"""
-TODO: This solution is slow. Can it be sped up?
-"""
-
-
 def longest_palindrome(s):
     if is_palindrome(s):
         return s
     longest = s[0]
     seen_values = {s[0]}
+    last_value = s[0]
+    last_value_sequence_length = 1
     for i in range(1, len(s)):
-        if s[i] in seen_values:
-            candidate = find_longest_palindrome_starting_with_last_value(s[s.find(s[i]):i+1])
-            if len(candidate) > len(longest):
-                longest = candidate
+        c = s[i]
+        if c == last_value:
+            last_value_sequence_length += 1
         else:
-            seen_values.add(s[i])
+            # check for long sequence palindrome at boundaries between different values
+            if last_value_sequence_length > len(longest):
+                longest = s[i-last_value_sequence_length:i]
+
+            if c in seen_values:
+                # we've seen this value before and its not part of last value sequence, check for palindromes
+                candidate = find_longest_palindrome_starting_with_last_value(s[s.find(c):i+1])
+                if len(candidate) > len(longest):
+                    longest = candidate
+            else:
+                # new value, can't be a palindrome longer than the first value in the input
+                seen_values.add(c)
+
+            last_value_sequence_length = 1
+            last_value = c
+
+    # catch case like 'abbb' where longest is a sequence of repeated values at the end
+    if last_value_sequence_length > len(longest):
+        longest = s[len(s) - last_value_sequence_length:len(s)]
 
     return longest
 
