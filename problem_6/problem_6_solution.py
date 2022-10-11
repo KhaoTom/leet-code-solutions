c5d5a5c8688bd57c93820d38dddfe86cc1266c0e
Edwin DeNicholas <disruption999@hotmail.com>
2022-10-10 20:54:37 -0700

    Problem 6 accepted solution 1

--- problem_6/problem_6_solution.py
+++ problem_6/problem_6_solution.py
@@ -1,8 +1,11 @@
 def convert_to_zigzag_text(s, rows):
+    if rows == 1:
+        return s
+
     zig = True
     y = 0
     ztext = ['' for _ in range(rows)]
-    initial_zig_step = 0 if rows == 2 else -1
+
     for c in s:
         ztext[y] += c
         if zig:
@@ -12,9 +15,9 @@ def convert_to_zigzag_text(s, rows):
             else:
                 y += 1
         else:
-            if y - 1 <= 0:
+            if y == 0:
                 zig = True
-                y = 0
+                y = 1
             else:
                 y -= 1
 
