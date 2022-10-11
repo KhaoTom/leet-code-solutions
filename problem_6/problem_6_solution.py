9c0b5382153dfa63e4aa2149781254be7497f1b6
Edwin DeNicholas <disruption999@hotmail.com>
2022-10-10 20:48:15 -0700

    Problem 6 initial solution 1 failing case.

--- problem_6/problem_6_solution.py
+++ problem_6/problem_6_solution.py
@@ -1,2 +1,21 @@
 def convert_to_zigzag_text(s, rows):
-    return ''
+    zig = True
+    y = 0
+    ztext = ['' for _ in range(rows)]
+    initial_zig_step = 0 if rows == 2 else -1
+    for c in s:
+        ztext[y] += c
+        if zig:
+            if y + 1 >= rows:
+                zig = False
+                y -= 1
+            else:
+                y += 1
+        else:
+            if y - 1 <= 0:
+                zig = True
+                y = 0
+            else:
+                y -= 1
+
+    return ''.join(ztext)
--- problem_6/test_problem_6_solution.py
+++ problem_6/test_problem_6_solution.py
@@ -6,6 +6,11 @@ import problem_6.problem_6_solution as p6s
 @pytest.mark.parametrize(
     "s, rows, expected",
     [
+        ('A', 1, 'A'),
+        ('AB', 1, 'AB'),
+        ('ABC', 1, 'ABC'),
+        ('ABCD', 1, 'ABCD'),
+        ('ABCD', 2, 'ACBD'),
         ('PAYPALISHIRING', 3, 'PAHNAPLSIIGYIR'),
         ('PAYPALISHIRING', 4, 'PINALSIGYAHRPI'),
     ]
